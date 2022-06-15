e4c3710a5eee0b36263133aa3115ef227a361254
morenabaron <mbaron@udesa.edu.ar>
2022-06-15 18:23:40 -0300

    s

--- js/detallege.js
+++ js/detallege.js
@@ -1,21 +1,27 @@
-let qs = location.search;
-let qsObjetoLiteral = new URLSearchParams(qs);
 
+/*almacenamos la query string de la url en una variable let que la llamamos qs, a tarvés de location.search*/
+let qs = location.search;
+/*convertims la cadena de texto que obtuvimos arriba en un objeto literal para manipular*/
+let qsObjetoLiteral = new URLSearchParams(qs);
+/*obtenemos el valor de una clave de la query string con .get*/
 let nombreQS = qsObjetoLiteral.get ('id');
 
-
+/*seleccionamos la seccion que queremos modificar*/
 let seldg = document.querySelector('.sectionDetalleG');
 let proxy = 'https://api.allorigins.win/raw?url='
-let url =  'https://api.deezer.com/genre/${nombreQS}'
+let url =  `https://api.deezer.com/genre/${nombreQS}`
 
+/* "agarramos" esta seccion que seleccionamos, que recibe como parámetro el endpoint que es nuenstra url y el proxy que no cambia*/
 fetch (proxy + url) 
+/*primera promesa: recibe lo que le manda la API y lo transforma a un obj lit, recibe info y se lo manda a la segunda*/
 .then(function(response){
     return response.json();
 
 })
-
+/*recibe la info y se manipula aca*/
 .then(function(data){
     console.log(data);
+    
 
 
 let title = document.querySelector ('h2');
@@ -27,9 +33,34 @@ img.src =  data.picture_big;
 
 })
 
+
+
 .catch(function(error){
     console.log(error);
 })
+
+let url2 =  `https://api.deezer.com/genre/${nombreQS}/artist`
+let seldg2= document.querySelector('.sectionDetalleG1');
+
+fetch (proxy + url2) 
+.then(function(response){
+    return response.json();
+
+})
+
+.then(function(data){
+    console.log(data);
+
+
+
+})
+
+
+
+.catch(function(error){
+    console.log(error);
+})
+
 
 
 
--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ fetch (proxy + url)
         section.innerHTML +=      
                             `<article class="articlemarc">
                             <div>
-                               <a href="/detalle-canciones.html?idCanciones"><img class="marc" src="${track[i].artist.picture_big}" alt=""></a>
+                               <a href="/detalle-canciones.html?idCanciones=${track[i].id}"><img class="marc" src="${track[i].artist.picture_big}" alt=""></a>
                             </div>
                             <div>
                                 <h4>${track[i].title}</h4>
