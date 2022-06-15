938a36cf183bd50dd8eaec8ebdb9ee2c565df2ed
morenabaron <mbaron@udesa.edu.ar>
2022-06-15 20:29:56 -0300

    a

--- js/cancion.js
+++ js/cancion.js
@@ -3,12 +3,13 @@ let qs = location.search;
 let qsObjetoLiteral = new URLSearchParams(qs);
 
 let nombreQS = qsObjetoLiteral.get ('idCanciones');
+console.log(nombreQS);
 
 let seldc = document.querySelector('.sectionDC');
 let proxy = 'https://api.allorigins.win/raw?url='
-let url =  `https://api.deezer.com/track/3135556${nombreQS}`
+let url =  `https://api.deezer.com/track/${nombreQS}`
 
-console.log(nombreQS);
+
 
 fetch (proxy + url)
     .then (function(response){
@@ -16,11 +17,21 @@ fetch (proxy + url)
 
     })
     .then (function(data){
-        console.log(data);
-
-    let track = data.tracks.data
-    console.log(track);
-
+        console.log(data)
     
+
+
+let img = document.querySelector('. detalleC');
+let title = document.querySelector('.titulodoja'); 
+let cancion = document.querySelector('.nombrecanciondoja');
+let album = document.querySelector('.nombrealbum');
+
+img.src = data.album.cover_big 
+title.innerHTML = data.artist.name
+cancion.innerHTML = data.title 
+album.innerHTML = data.album.title 
+
+
+
 })
 
