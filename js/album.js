6ebeeac843f84107d841f0a1e4be85dc2213f512
moradiaz <moradiaz@udesa.edu.ar>
2022-06-18 19:55:45 -0300

    fix button add favorites

--- js/album.js
+++ js/album.js
@@ -13,12 +13,11 @@ console.log(nombreQS);
 fetch (proxy + url)
     .then (function(response){
         return response.json()
-
     })
     .then (function(data){
         console.log(data);
 
-        let track = data.data
+        let track = data.albums.data
         console.log(track);
         
         for (let i = 0; i < 1; i++) {
@@ -26,30 +25,17 @@ fetch (proxy + url)
                                 `<article class="cajaTaylor sectionDA">
                                 <img class="imagenTaylor" src="${track[i].cover_big}"alt=""> </a>
                              </article>`
-        }
-       
-        
-        section = document.querySelector('.cajaTaylor')
-        for (let i = 0; i < 1; i++) {
-            console.log(track[i].id); 
-             section.innerHTML +=
-                        `<article class="textoTaylor">
-                            <h1 class="tituloTaylor">${track[i].artist.name}</h1>
+
+                            `<article class="textoTaylor">
+                                <h1 class="tituloTaylor">${track[i].artist.name}</h1>
 
                                 <h2 class="nombrealbumtaylor">${track[i].data.title}</h2>
-            
-                            <h4 class="infoTaylor generotaylor">${track[i].data.genre_id}</h4>
-                             <h4 class="infoTaylor añotaylor">${track[i].data.release_date}</h4>
-                       </article>`
+                                    <h4 class="infoTaylor generotaylor">${track[i].data.genre_id}</h4>
+                                    <h4 class="infoTaylor añotaylor">${track[i].data.release_date}</h4>
+                            </article>`
         
-
-  }
-
+        }
     })
-
-
-
-
     .catch(function(error){
         console.log(error);
     })
